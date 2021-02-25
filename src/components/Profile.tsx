5cc8331b60e69e7432becaea804d5f753eb24a08
Ermelinda <ermelinda.ti05n@gmail.com>
2021-02-25 02:01:56 -0300

    chore: Aula 02 - criando projeto com Next e componentes de barra e perfil

---
+++ src/components/Profile.tsx
@@ -0,0 +1,19 @@
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile() {
+  return (
+    <div className={styles.profileContainer}>
+      <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSM8ziIN18mTbi5Y3MzTHNk95s05ISkxaIvmQ&usqp=CAU"
+        alt="Ermelinda"
+      />
+      <div>
+        <strong>Ermelinda Abreu</strong>
+        <p>
+          <img src="icons/level.svg" alt="Level" />
+          Level 1
+        </p>
+      </div>
+    </div>
+  );
+}
